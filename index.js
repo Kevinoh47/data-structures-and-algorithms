9e3b223daf6bfbe5fc40eec3f54bf316b827ebeb
Kevinoh47 <kohok47@gmail.com>
2018-10-10 13:01:02 -0700

    added tests, image, updates to readme

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const insertShiftArray = require('./array_shift.js');
 const binarySearch = require('./array_binary_search.js');
 
 let myOrderedArr = [47, 49, 55, 56, 60, 70];
 let mySearchKey = 56;
 
-console.log(binarySearch(myOrderedArr, mySearchKey));
+console.log('binarySearch', binarySearch(myOrderedArr, mySearchKey));
+console.log('insertShiftArray', insertShiftArray(myOrderedArr, mySearchKey));
