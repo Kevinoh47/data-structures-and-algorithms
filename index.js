c1ca0d45b018163043bdfb0acf8e13d165e149d8
Kevinoh47 <kohok47@gmail.com>
2018-11-15 11:39:20 -0800

    BinaryTree and BinarySearchTree with insert code

--- index.js
+++ index.js
@@ -6,22 +6,59 @@
 // const Node = require('./node.js');
 // const StackAndQueue = require('./StacksAndQueues/stacks-and-queues.js');
 
+// const {Queue} = require('./queueWithStacks/queue-with-stacks.js');
 
-const {Queue} = require('./queueWithStacks/queue-with-stacks.js');
+const {BinaryTree, BinarySearchTree} = require('./tree/tree.js');
 
-let myPseudoQueue = new Queue();
+let myBTree = new BinaryTree();
 
-myPseudoQueue.enqueue(1);
-myPseudoQueue.enqueue(2);
-myPseudoQueue.enqueue(3);
-myPseudoQueue.enqueue(4);
-myPseudoQueue.enqueue(5);
-myPseudoQueue.enqueue(6);
-myPseudoQueue.enqueue(7);
+myBTree.insert(47);
+myBTree.insert(23);
+myBTree.insert(26);
+myBTree.insert(49);
+myBTree.insert(1);
+myBTree.insert(0);
+myBTree.insert(142);
+myBTree.insert(99);
+myBTree.insert(66);
+myBTree.insert(33);
+myBTree.insert(199);
+myBTree.insert(102);
+myBTree.insert(47); // regular BTree would allow dups
 
-console.log('MY PSEUDO CUEUE', myPseudoQueue.stack1);
+console.log('MyBTree', myBTree);
 
-console.log('dequeue....', myPseudoQueue.dequeue());
+let myBSearchTree = new BinarySearchTree();
+myBSearchTree.insert(47);
+myBSearchTree.insert(23);
+myBSearchTree.insert(26);
+myBSearchTree.insert(49);
+myBSearchTree.insert(1);
+myBSearchTree.insert(0);
+myBSearchTree.insert(142);
+myBSearchTree.insert(99);
+myBSearchTree.insert(66);
+myBSearchTree.insert(33);
+myBSearchTree.insert(199);
+myBSearchTree.insert(102);
+myBSearchTree.insert(47); //  BST should not allow dups
+
+console.log('myBSearchTree', myBSearchTree);
+
+
+// let myPseudoQueue = new Queue();
+
+// myPseudoQueue.enqueue(1);
+// myPseudoQueue.enqueue(2);
+// myPseudoQueue.enqueue(3);
+// myPseudoQueue.enqueue(4);
+// myPseudoQueue.enqueue(5);
+// myPseudoQueue.enqueue(6);
+// myPseudoQueue.enqueue(7);
+
+// console.log('MY PSEUDO CUEUE', myPseudoQueue.stack1);
+
+// console.log('dequeue....', myPseudoQueue.dequeue());
 
 // let myOrderedArr = [47, 49, 55, 56, 60, 70];
 // let mySearchKey = 56;
---
+++ tree/tree.js
@@ -0,0 +1,89 @@
+'use strict';
+
+class BtNode {
+  constructor(key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+    this.count = 0;
+  }
+
+  insert(key) {
+    let newNode = new BtNode(key);
+
+    if (this.root === null) {
+      this.root = newNode;
+      this.count = 1;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+
+    if (node.left === null) {
+      node.left = newNode;
+      this.count++;
+    }
+    else if (node.right === null) {
+      node.right = newNode;
+      this.count++;
+    }
+    else  {
+      // if the node is full, pick right or left and continue looking for an available leaf spot.
+      const leftOrRight = ((Math.floor(Math.random() * 2) == 0)) ? node.left : node.right;
+
+      this.insertNode(leftOrRight, newNode);
+    }
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+    this.count = 0;
+  }
+
+  insert(key) {
+    let newNode = new BtNode(key);
+
+    if (this.root === null) {
+      this.root = newNode;
+      this.count = 1;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (node.key === newNode.key) {
+      return;
+    }
+    else if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+        this.count++;
+      }
+      else {
+        this.insertNode(node.left, newNode);
+      }
+    }
+    else {
+      if (node.right === null) {
+        node.right = newNode;
+        this.count++;
+      }
+      else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
+
+module.exports = {BinaryTree, BinarySearchTree};
