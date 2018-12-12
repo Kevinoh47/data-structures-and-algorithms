ef6c59981f255ef88a49ced7c7c1df2df0876869
Kevinoh47 <kohok47@gmail.com>
2018-12-11 17:25:08 -0800

    added tests and a bit more functionality

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {BinaryTree, BinarySearchTree} = require('./tree/tree.js');
 // const myStr = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
 // console.log(JSON.stringify(repeatedWord(myStr)));
 
-const myStr2 = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
+const myStr2 = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only... of of... was was was';
 
 console.log(JSON.stringify(repeatedWord(myStr2)));
 
--- repeatedWord/repeated-word.js
+++ repeatedWord/repeated-word.js
@@ -2,37 +2,49 @@
 
 
 let repeatedWord = (str) => {
-  // regex from https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
-  const stripPunc = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').toLowerCase();
+  // regex modified from https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
+  const stripPunc = str.replace(/[.,/#!$%^&*;:{}=\-_`~()–]/g,'').replace('  ',' ').trim().toLowerCase();
+
   const myStrArr = stripPunc.split(' ');
   const myHash = {};
   let firstDup = false;
-  let dupsInfo = {};
-  let highestDupCount = 0, highestDupWord = '';
+  let highestDupCount = 0;
+  let highestDupWords = [];
+  let dupWordCount = 0;
 
   myStrArr.forEach(function(e){
     if (myHash[e] === undefined) { 
       myHash[e]=1;
     }
     else {
+      
       if (!firstDup) { 
         firstDup = true;
-        dupsInfo.firstDuplicate = e;
+        myHash.firstDuplicate = e;
       }
      
-      myHash[e] = ++myHash[e];  
-      dupsInfo[e] = myHash[e];
+      myHash[e] = ++myHash[e]; 
+      
+      // only count word for dup count once
+      if (myHash[e] ===2) {
+        dupWordCount++;
+      }
 
-      if (myHash[e] > highestDupCount) {
+      if (myHash[e] === highestDupCount) {
+        highestDupWords.push(e);
+      }
+      else if (myHash[e] > highestDupCount) {
         highestDupCount = myHash[e];
-        highestDupWord = e;
+        highestDupWords = [];
+        highestDupWords.push(e);
       }
     }
-    dupsInfo.highestDupWord = highestDupWord;
-    dupsInfo.highestDupCount = highestDupCount;
+    myHash.highestDupWords = highestDupWords;
+    myHash.highestDupCount = highestDupCount;
+    myHash.dupWordCount = dupWordCount;
   });
 
-  return dupsInfo;
+  return myHash;
 };
 
 module.exports = repeatedWord;
