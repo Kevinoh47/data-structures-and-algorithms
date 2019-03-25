0b84bdb5ac55e2b3bd32ce5fbccd7153a837b64e
Kevinoh47 <kohok47@gmail.com>
2019-03-25 12:43:22 -0700

    minor cleanup

--- treeIntersection/tree-intersection.js
+++ treeIntersection/tree-intersection.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const {BinaryTree} = require('../tree/tree.js');
-
-const myHash = {};
-
 // traverse tree1, populating the hash table with keys named for the node values, with source tree1 + a counter for the key value (remember that regular binary trees can have duplicate values)
 
 // traverse tree2, pushing values into the hash. If the value already has a key in the hash table, then we signify in the value that it is part of the intersection. Or possibly we write it to a result array
