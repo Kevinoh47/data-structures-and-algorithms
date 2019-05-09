829521abcaa369494e8a025e0d2bf52937dee703
Kevinoh47 <kohok47@gmail.com>
2019-05-09 10:25:03 -0700

    refactored max sub array to use array.reduce

--- post_grad_practice/practice-maximum-subarray.js
+++ post_grad_practice/practice-maximum-subarray.js
@@ -12,9 +12,9 @@ let maxSubArray = nums => {
 
   for (let i = 0; i < nums.length; i++) {
     let currentIndex = i;
-    //console.log({currentIndex});
+
     let currentSum = nums[currentIndex];
-    //console.log({currentSum});
+
     while (currentIndex >= 0) {
       if (currentSum > max) {
         max = currentSum;
@@ -31,3 +31,36 @@ console.log(maxSubArray(input));
 
 input = [-2,1,0];
 console.log(maxSubArray(input));
+
+console.log('\n ... rewrite using reducer ... \n');
+/**
+ * much less memory than the previous version...
+ * Success
+Details
+Runtime: 1484 ms, faster than 5.12% of JavaScript online submissions for Maximum Subarray.
+Memory Usage: 35.1 MB, less than 78.99% of JavaScript online submissions for Maximum Subarray.
+ * @param {*} nums 
+ */
+
+let maxSubArr2 = nums => {
+  let max = nums[0];
+
+  nums.reduce((prev, curr, currIndex) => {
+    let currSum = curr;
+    while(currIndex >=0) {
+      if (currSum > max) {
+        max = currSum;
+      }
+      currIndex--;
+      currSum = currSum + nums[currIndex];
+    }
+  }, max);
+
+  return max;
+};
+
+input = [-2,1,-3,4,-1,2,1,-5,4];
+console.log(maxSubArr2(input));
+
+input = [-2,1,0];
+console.log(maxSubArr2(input));
