5b9ecb3f14b4a0981145aaeb1326754f35b78129
Kevinoh47 <kohok47@gmail.com>
2019-08-08 10:12:44 -0700

    refactor

--- post_grad_practice/practice-longest-prefix.js
+++ post_grad_practice/practice-longest-prefix.js
@@ -16,37 +16,24 @@ let longestCommonPrefix = strsArr => {
   strsArr.forEach(s => {
     let currStrLen = s.length;
 
-    // console.log({currStrLen});
-
     if (currStrLen < shortestString) {
       shortestString = currStrLen;
     }
   });
 
-  // console.log({shortestString});
-
   while (counter <= shortestString -1) {
-    
-    // console.log({counter});
 
     for (let i = 0; i < strsArr.length; i++) {
 
       let startingVal = strsArr[0].charAt(counter); 
-      // console.log({startingVal});
-
       let currentWord = strsArr[i];
-      // console.log({currentWord});
-
       let currentChar = currentWord.charAt(counter);
-      // console.log({currentChar});
 
       if (currentChar !== startingVal) {
-        // console.log( `currentChar ${currentChar} differs from startingVal ${startingVal}`);
         return prefix;
       }
       // we have made it to the final comparison
       else if ( i === strsArr.length - 1 ) {
-        // console.log( `adding currentChar ${currentChar} to prefix ${prefix}`);
         prefix = prefix.concat(currentChar); 
       }
     }
@@ -55,26 +42,24 @@ let longestCommonPrefix = strsArr => {
   return prefix;
 };
 
-console.log('\n ... test 1 ... \n');
+console.log('\n ... test 1 expects fl... \n');
 let testArr = ['flower','flow','flight'];
-
 let result = longestCommonPrefix(testArr);
-
 console.log({result});
 
-console.log('\n ... test 2 ... \n');
+console.log('\n ... test 2 expects empty string... \n');
 
 testArr = ['abc','def','ge'];
 result = longestCommonPrefix(testArr);
 console.log({result});
 
-console.log('\n ... test 3 ... \n');
+console.log('\n ... test 3 expects abcd... \n');
 
 testArr = ['abcdefg','abcdzzz','abcdefg'];
 result = longestCommonPrefix(testArr);
 console.log({result});
 
-console.log('\n ... test 4 ... \n');
+console.log('\n ... test 4 expects abcd... \n');
 
 testArr = ['abcdefg','abcd','abcdefg'];
 result = longestCommonPrefix(testArr);
@@ -85,6 +70,72 @@ console.log('\n ... test 5 expects empty string... \n');
 testArr = ['aca','cba'];
 result = longestCommonPrefix(testArr);
 console.log({result});
+
+console.log('\n ... test 6 expects the entire first string abcd... \n');
+
+testArr = ['abcd', 'abcd', 'abcd'];
+result = longestCommonPrefix(testArr);
+console.log({result});
+
+
+/**
+ * Refactor based on https://leetcode.com/problems/longest-common-prefix/discuss/120133/Simple-JavaScript-solution. You don't need to worry about shortest string, because you return as soon as there is no match. just pick the first one to iterate over.
+ * 
+ * Success
+Details
+Runtime: 52 ms, faster than 92.15% of JavaScript online submissions for Longest Common Prefix.
+Memory Usage: 35.6 MB, less than 21.88% of JavaScript online submissions for Longest Common Prefix.
+
+ */
+let longestCommonPrefix2 = strsArr => {
+
+  if (!strsArr.length) {return '';}
+
+  // iterate over the length of the first string. Whether it is shortest or not doesn't matter
+  for (let i = 0; i < strsArr[0].length; i++) {
+
+    // handles almost all cases, including no prefix
+    for (let str of strsArr) {
+      if ( str[i] !== strsArr[0][i]) {
+        return str.slice(0,i);
+      }
+    }
+  }
+  // edge case: all inputs are exactly the same:
+  return strsArr[0];
+};
+
+console.log('\n ... refactor1 test 1 expects fl ... \n');
+testArr = ['flower','flow','flight'];
+result = longestCommonPrefix2(testArr);
+console.log({result});
+
+console.log('\n ... refactor1 test 2 expects empty string... \n');
+testArr = ['abc','def','ge'];
+result = longestCommonPrefix2(testArr);
+console.log({result});
+
+console.log('\n ... refactor1 test 3 expects abcd... \n');
+testArr = ['abcdefg','abcdzzz','abcdefg'];
+result = longestCommonPrefix2(testArr);
+console.log({result});
+
+console.log('\n ... refactor1 test 4 expects abcd... \n');
+testArr = ['abcdefg','abcd','abcdefg'];
+result = longestCommonPrefix2(testArr);
+console.log({result});
+
+console.log('\n ... refactor1 test 5 expects empty string... \n');
+testArr = ['aca','cba'];
+result = longestCommonPrefix2(testArr);
+console.log({result});
+
+console.log('\n ... refactor1 test 6 expects the entire first string abcd... \n');
+testArr = ['abcd', 'abcd', 'abcd'];
+result = longestCommonPrefix2(testArr);
+console.log({result});
+
+
 
 
 
