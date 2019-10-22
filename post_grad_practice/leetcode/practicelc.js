46e5d87320b3c91bf16ef6a4a9678d8093856188
Kevinoh47 <kohok47@gmail.com>
2019-10-22 14:08:57 -0700

    house robber but no robbing adjacent houses

--- post_grad_practice/leetcode/practicelc.js
+++ post_grad_practice/leetcode/practicelc.js
@@ -369,3 +369,249 @@ words = ['word','world','row'];
 order = 'worldabcefghijkmnpqstuvxyz';
 myResult = wordsAreInOrder2(words, order);
 console.log('expect false: ', myResult);
+
+console.log(`\n ... House Robber ... \n`);
+/**
+ * House Robber
+ * https://leetcode.com/problems/house-robber/
+ * 
+ * given an array of numbers, find the highest sum of subsets that are not adjacent to each other.arrow
+ * 
+ */
+// ooops this one misunderstands the problem. See the third condition:
+function largestNonAdjacentSum (arr) {
+
+  let output = arr.reduce((prev, curr, currIdx) => {
+    if(currIdx % 2 !== 0) {
+      prev.odds += (curr > 0) ? curr : 0 ;
+    }  else {
+      prev.evens +=  (curr > 0) ? curr : 0 ;
+    }
+    return prev;
+  
+  }, {odds:0, evens:0});
+
+  return Math.max(output.odds, output.evens);
+}
+
+console.log('expect 4:', largestNonAdjacentSum([1,2,3,1]));
+console.log('expect 12:', largestNonAdjacentSum([2,7,9,3,1]));
+console.log('oops expect 4 but returns 3:', largestNonAdjacentSum([2,1,1,2])); // returns 3
+
+console.log(`\n ... House Robber version 2 ... \n`);
+/**
+ * Notes: 
+ * We can get all possibilities with two cases, by starting with the first value, skipping the next one, then adding i = 2 + all subsequent evens, and adding i = 3 + all subsequent odds.
+ * there are two cases: 
+ * case1: i=0 + i>=2 && i is even
+ * case2: i=0 + i>=3 && i is odd
+ */
+function largestNonAdjacentSum2 (arr) {
+
+  let output = arr.reduce((prev, curr, currIdx) => {
+    if(currIdx === 0) { 
+      prev = {case1:curr, case2:curr};
+    } else if (currIdx >= 2 && currIdx % 2 === 0) {
+      prev.case1 += (curr > 0) ? curr : 0 ;
+    } else if (currIdx >= 3 && currIdx % 2 !== 0) {
+      prev.case2 +=  (curr > 0) ? curr : 0 ;
+    }
+    return prev;
+  
+  }, {case1:0, case2:0});
+
+  return Math.max(output.case1, output.case2);
+}
+
+console.log('expect 4:', largestNonAdjacentSum2([1,2,3,1]));
+console.log('expect 12:', largestNonAdjacentSum2([2,7,9,3,1]));
+console.log('expect 4:', largestNonAdjacentSum2([2,1,1,2])); 
+console.log('expect 10:', largestNonAdjacentSum2([2,1,3,4,3,1,2])); 
+console.log('ooops expected 2 returned 1:', largestNonAdjacentSum2([1,2])); 
+
+console.log(`\n ... House Robber version 3 ... \n`);
+/**
+ * Notes: 
+ * We can get all possibilities with two cases, by starting with the first value, skipping the next one, then adding i = 2 + all subsequent evens, and adding i = 3 + all subsequent odds.
+ * there are two cases: 
+ * case1: i=0 + i>=2 && i is even
+ * case2: i=0 + i>=3 && i is odd
+ */
+function largestNonAdjacentSum3 (arr) {
+
+  let output = arr.reduce((prev, curr, currIdx) => {
+    if (currIdx % 2 === 0) {
+      prev.case1 += (curr > 0) ? curr : 0 ;
+    } else {
+      prev.case2 +=  (curr > 0) ? curr : 0 ;
+    }
+    return prev;
+
+  
+  }, {case1:0, case2:0});
+
+  return Math.max(output.case1, output.case2);
+}
+
+console.log('expect 4: ', largestNonAdjacentSum3([1,2,3,1]));
+console.log('expect 12: ', largestNonAdjacentSum3([2,7,9,3,1]));
+console.log('expect 4: ', largestNonAdjacentSum3([2,1,1,2])); 
+console.log('expect 10: ', largestNonAdjacentSum3([2,1,3,4,3,1,2])); 
+console.log('expect 2: ', largestNonAdjacentSum3([1,2])); 
+
+console.log(`\n ... House Robber version 4 ... \n`);
+/**
+ * Notes: 
+ * We can get all possibilities with two cases, by starting with the first value, skipping the next one, then adding i = 2 + all subsequent evens, and adding i = 3 + all subsequent odds.
+ * there are two cases: 
+ * case1: i=0 + i>=2 && i is even
+ * case2: i=0 + i>=3 && i is odd
+ */
+function largestNonAdjacentSum4 (arr) {
+
+  let output = arr.reduce((prev, curr, currIdx) => {
+    if(currIdx >= 0 && currIdx % 2 === 0) { 
+      prev.evensFrom0 += (curr > 0) ? curr : 0 ;
+    } 
+    else if(currIdx >= 0 && currIdx % 2 !== 0) {
+      prev.oddsFrom0 += (curr > 0) ? curr : 0 ;
+    } 
+    else if(currIdx >= 1 && currIdx % 2 === 0) { 
+      prev.evensFrom1 += (curr > 0) ? curr : 0 ;
+    } 
+    else if(currIdx >= 1 && currIdx % 2 !== 0) {
+      prev.oddsFrom1 += (curr > 0) ? curr : 0 ;
+    } 
+    return prev;
+  }, {evensFrom0:0, oddsFrom0:0, evensFrom1:0, oddsFrom1:0});
+  // console.log({output});
+  return Math.max(output.evensFrom0, output.oddsFrom0, output.evensFrom1, output.oddsFrom1);
+}
+
+console.log('expect 4:', largestNonAdjacentSum4 ([1,2,3,1]));
+console.log('expect 12:', largestNonAdjacentSum4 ([2,7,9,3,1]));
+console.log('ooops expect 4 returns 3:', largestNonAdjacentSum4 ([2,1,1,2])); 
+console.log('expect 10:', largestNonAdjacentSum4 ([2,1,3,4,3,1,2])); 
+console.log('expected 2:', largestNonAdjacentSum4 ([1,2])); 
+
+console.log(`\n ... House Robber version 5 ... \n`);
+/**
+ * Notes: 
+ */
+function largestNonAdjacentSum5 (arr) {
+
+  let left = 0, right = arr.length-1;
+  let sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0;
+
+  if (arr.length === 0) {return 0;}
+  // case array length 1 or 2
+  else if (right < 2) {
+    return Math.max(arr[left], arr[right]);
+  }
+
+  // test left 0, right is last
+  while(right - left >= 0) {
+    if(right - left === 0) {
+      sum1 = sum1 + arr[left]
+    }
+    else if (right - left >=2) {
+      sum1 = sum1 + arr[left] + arr[right];
+    }
+    
+    left = left + 2;
+    right = right -2;
+  }
+
+  // test left 1, right is last
+  left = 1, right = arr.length-1;
+
+  while(right - left >= 0) {
+    if(right - left === 0) {
+      sum2 = sum2 + arr[left];
+    }
+    else if (right - left >=2) {
+      sum2 = sum2 + arr[left] + arr[right];
+    }
+    left = left + 2;
+    right = right -2;
+  }
+
+  // test left 0, right is penultimate
+  left = 0, right = arr.length-2;
+
+  while(right - left >= 0) {
+    if(right - left === 0) {
+      sum3 = sum3 + arr[left];
+    }
+    else if (right - left >=2) {
+      sum3 = sum3 + arr[left] + arr[right];
+    }
+    left = left + 2;
+    right = right -2;
+  }
+
+  // test left 1, right is penultimate
+  left = 1, right = arr.length-2;
+
+  while(right - left >= 0) {
+    if(right - left === 0) {
+      sum4 = sum4 + arr[left];
+    }
+    else if (right - left >=2) {
+      sum4 = sum4 + arr[left] + arr[right];
+    }
+    left = left + 2;
+    right = right -2;
+  }
+
+  return(Math.max(sum1, sum2, sum3, sum4));
+}
+
+console.log('expect 4: ', largestNonAdjacentSum5 ([1,2,3,1]));
+console.log('expect 12: ', largestNonAdjacentSum5 ([2,7,9,3,1]));
+console.log('expect 4: ', largestNonAdjacentSum5 ([2,1,1,2])); 
+console.log('expect 10: ', largestNonAdjacentSum5 ([2,1,3,4,3,1,2])); 
+console.log('expect 2: ', largestNonAdjacentSum5 ([1,2])); 
+console.log('expected 14 -- oops: ', largestNonAdjacentSum5 ([4,1,2,7,5,3,1])); 
+
+/**
+ * 
+ * OK none of that works...
+ * translated from the provided solution:
+ * 
+ * Runtime: 48 ms, faster than 90.43% of JavaScript online submissions for House Robber.
+Memory Usage: 33.7 MB, less than 85.71% of JavaScript online submissions for House Robber.
+ *  
+ Unfortunately, I don't quite understand how this guarrantees that we don't rob adjacent houses. 
+ * */ 
+
+
+console.log(`\n ... House Robber, inspired by the from the provided solution ... \n`);
+
+function largestNonAdjacentSum6 (nums) {
+
+  if(nums.length === 0) {return 0;}
+  else if (nums.length <= 2) {return Math.max(nums[0],nums[nums.length-1]);}
+  else if(nums.length === 3) {return Math.max(nums[0]+nums[2], nums[1]);}
+  else {
+    let prevMax = 0,  currMax = 0;
+    for (let i = 0; i < nums.length; i++) {
+      let temp = currMax;
+      currMax = Math.max(prevMax + nums[i], currMax);
+
+      console.log(temp, currMax);
+      prevMax = temp;
+    }
+    return currMax;
+  }
+}
+
+console.log('expect 4:', largestNonAdjacentSum6 ([1,2,3,1]));
+console.log('expect 12:', largestNonAdjacentSum6 ([2,7,9,3,1]));
+console.log('expect 4: ', largestNonAdjacentSum6 ([2,1,1,2])); 
+console.log('expect 10:', largestNonAdjacentSum6 ([2,1,3,4,3,1,2])); 
+console.log('expect 2:', largestNonAdjacentSum6 ([1,2])); 
+console.log('expect 14: ', largestNonAdjacentSum6 ([4,1,2,7,5,3,1])); 
+
+
+
